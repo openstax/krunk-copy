eac25a0d96f4ff3cc6c4a61e48c32aba48a629c4
m1yag1 <mike.arbelaez@rice.edu>
2018-10-10 15:59:35 -0500

    bump version (#2)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = ['docopt', 'bs4', 'python-dateutil', 'aiohttp']
 
 setup(
     name='krunk-copy',
-    version='0.0.3',
+    version='0.0.4',
     description='A utility that allows you to download pdfs '
                 'based on collection_ids',
     long_description='',
